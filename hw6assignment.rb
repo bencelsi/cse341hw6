9f5bc46799ea3b1d538f57f4d30a81ba3050a80e
Ben Celsi <bencelsi@uw.edu>
2018-02-27 01:08:36 -0800

    what

--- hw6assignment.rb
+++ hw6assignment.rb
@@ -7,43 +7,68 @@
 #
 
 
-require_relative './hw6gragdadaphics'
 
 class MyPiece < Piece
-  # The constant All_My_Pieces should be declared here
 	All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-               [[0, 0], [0, -1], [0, 1], [0, 2]]],
-               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
-			   [[[0, 0], [-1, 0], [1, 0], [2, 0],[-2, 0]], # longer long
-               [[0, 0], [0, -1], [0, 1], [0, 2], [0,-2]]]
-               rotations([[0,0],[1,0],[0,1]]), #small L
-			   rotations([[0,0],[-1,0],[-1,1],[0,1],[1,0]])] # thumbs up
-  # your enhancements here
-  
-	def initialize(x,y,c)
+        #rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+		#[[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+		#[[0, 0], [0, -1], [0, 1], [0, 2]]],
+        #rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+	    #rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+	    #rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+	    #rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]),
+	    rotations([[0,0],[-1,0],[-1,1],[0,1],[1,0]])] # thumbs up
+
+	def initialize(point_array, board)
 		super(point_array, board)
-		
 	end
 	
 	def self.next_piece (board)
-		Piece.new(All_My_Pieces.sample, board)
+		MyPiece.new(All_My_Pieces.sample, board)
 	end
+	
 	
 end
 
 class MyBoard < Board
+	def initialize (game)
+		super(game)
+		@current_block = MyPiece.next_piece(self)
+	end
   # your enhancements here
-
+	def rotate_upside_down
+    if !game_over? and @game.is_running?
+      @current_block.move(0, 0, -2)
+    end
+    draw
+  end
 end
 
 class MyTetris < Tetris
-  # your enhancements here
 
+	def abc
+		print "abc"
+	end
+	def initialize
+		super
+	end
+	
+	def set_board
+		@canvas = TetrisCanvas.new
+		@board = MyBoard.new(self)
+		@canvas.place(@board.block_size * @board.num_rows + 3,
+        @board.block_size * @board.num_columns + 6, 24, 80)
+		@board.draw
+	end
+	def key_bindings
+		super
+		@root.bind('z', proc {self.new_game})
+		
+		@root.bind('u', proc {@board.rotate_upside_down}) 
+		abc
+	end
+	
+	
 end
 
 
