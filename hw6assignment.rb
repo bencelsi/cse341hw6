cb73629ab41a2bb1ce4636465a500789e2704316
Ben Celsi <bencelsi@uw.edu>
2018-03-01 14:59:10 -0800

    added cheat

--- hw6assignment.rb
+++ hw6assignment.rb
@@ -1,18 +1,21 @@
 # University of Washington, Programming Languages, Homework 6, hw6runner.rb
 
-# This is the only file you turn in, so do not modify the other files as
-# part of your solution.
-
-#Notes on provided:
-#
-
 class MyPiece < Piece
 	All_My_Pieces = All_Pieces.push(
-		rotations([[0,0],[-1,0],[-1,1],[0,1],[1,0]]),   # thumbs up
-		[[[0, 0], [-1, 0], [1, 0], [2, 0], [-2, 0]],   # longer long
-		[[0, 0], [0, -1], [0, 1], [0, 2], [0, -2]]],       
-		rotations([[0,0],[1,0],[0,1]]))   #elbow 
-		
+		rotations([[0,0],[-1,0],[-1,1],[0,1],[1,0]]), # thumbs up
+		[[[0,0],[-1,0],[1,0],[2,0],[-2,0]], # longer long
+		[[0,0],[0,-1],[0,1],[0,2],[0,-2]]],       
+		rotations([[0,0],[1,0],[0,1]]))   	#elbow 
+	
+	#All_My_Pieces = [
+	#	rotations([[0,0],[-1,0],[-1,1],[0,1],[1,0]]), 	# temple
+    #   rotations([[-2, 1], [-1, 1], [0, 1], [1, 1],	# jungle 
+	#	[-2,0],[-1,0],[0,0],[1,0],[-1,-1],[0,-1],[1,-1]]),
+	#	rotations([[0, 0], [0, 1], [1, 0], [1, 1]]), 	# boiler 
+	#	rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), 	# survey  
+	#	[[0,0]]]   										#prison
+
+	
 	def initialize(point_array, board)
 		super(point_array, board)
 	end
@@ -26,6 +29,7 @@ end
 class MyBoard < Board
 	def initialize (game)
 		super(game)
+		@cheating = false
 		@current_block = MyPiece.next_piece(self)
 	end
 	
@@ -42,7 +46,12 @@ class MyBoard < Board
 	end
 	
 	def next_piece
-		@current_block = MyPiece.next_piece(self)
+		if @cheating
+			@current_block = MyPiece.new([[0,0]], self)
+			@cheating = false
+		else
+			@current_block = MyPiece.next_piece(self)
+		end
 		@current_pos = nil
 	end
 	
@@ -52,6 +61,16 @@ class MyBoard < Board
 		end
 		draw
 	end
+	
+	def cheat
+		if @score > 10 and !@cheating
+			@score -= 10
+			@cheating = true
+		end
+	end
+	
+	
+	
 end
 
 class MyTetris < Tetris
@@ -67,6 +86,8 @@ class MyTetris < Tetris
 	def key_bindings
 		super
 		@root.bind('u', proc {@board.rotate_upside_down}) 
+		@root.bind('c', proc {@board.cheat}) 
+		
 	end
 end
 
